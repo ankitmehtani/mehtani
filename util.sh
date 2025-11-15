4533881f2b161acb5cf48dc247312dec8c5b7c70
root <root@ip-172-31-22-237.ec2.internal>
2025-11-15 09:21:43 +0000

    newcommit

---
+++ util.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Log rotations script: Archive the logs older than a specified number of days.
+
+mkdir -p /root/logs/archive
+
+find /root/logs -type f -name "*.log" -mtime +3 |while read -r Logfile; do
+    gzip "$Logfile"
+done
+mv /root/logs/*.log.gz /root/logs/archive/
+echo "Log rotation completed: Archived logs older than 3 days."
